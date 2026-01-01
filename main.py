05311495545fd9f92b58de743e2f3d9c31c480ee
PandaBump <stokesk.swe@gmail.com>
2026-01-01 03:09:24 -0500

    add get_book_text function

---
+++ main.py
@@ -0,0 +1,13 @@
+file_contents = ""
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    
+    return file_contents
+
+def main():
+
+    get_book_text("books/frankenstein.txt")
+    print(file_contents)
+
+main()
